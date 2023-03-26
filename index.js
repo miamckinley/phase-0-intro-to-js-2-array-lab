09384cb95c35e68c75bbd69743846ff9d2a5f444
Mia McKinley <mia.k.mckinley52@gmail.com>
2023-03-26 17:59:01 -0400

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (name) {
+cats.push("Ralph");
+}
+function destructivelyPrependCat (name) {
+cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat (name) {
+cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat (name) {
+cats.shift("Milo");
+}
+function appendCat(name) {
+const Cats = cats.slice();
+Cats.push(name);
+return Cats;
+}
+function prependCat(name) {
+const Cats = cats.slice();
+Cats.unshift(name);
+return Cats;
+}
+function removeLastCat(name) {
+const Cats = cats.slice();
+Cats.pop(name);
+return Cats;
+}
+function removeFirstCat(name) {
+const Cats = cats.slice();
+Cats.shift(name);
+return Cats;
+}
